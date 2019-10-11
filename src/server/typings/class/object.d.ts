871cd7a5a00b1efb7e5fb1ae49cebd20f1466f0c
Timur Popov <timur2915@gmail.com>
2019-10-11 13:33:42 +0300

    Add initial serverside Object class declaration

---
+++ src/server/typings/class/object.d.ts
@@ -0,0 +1,21 @@
+declare namespace Server {
+  /** @customConstructor Object */
+  class GameObject extends Element {
+    /**
+     * Creates an object in the GTA world.
+     * - Note: Dynamic objects do not automatically have physics applied to them. Use setElementVelocity(object, 0, 0, 0) to fix this.
+     * @param modelid a whole integer specifying the GTASA object model ID.
+     * @param x a floating point number representing the X coordinate on the map.
+     * @param y a floating point number representing the Y coordinate on the map.
+     * @param z a floating point number representing the Z coordinate on the map.
+     * @param rx a floating point number representing the rotation about the X axis in degrees.
+     * @param ry a floating point number representing the rotation about the Y axis in degrees.
+     * @param rz a floating point number representing the rotation about the Z axis in degrees.
+     * @param [isLowLOD=false] unknown
+     * @returns
+     * @see https://wiki.mtasa.com/wiki/CreateObject
+     **/
+    constructor(modelid: number, x: number, y: number, z: number, rx: number, ry: number, rz: number, isLowLOD?: boolean);
+
+  }
+}
