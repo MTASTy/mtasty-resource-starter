debd28ca8d8c16575a44eea43ab8157d24a9d45f
Evgeniy Shtro <augustalonso@gmail.com>
2019-10-12 15:35:28 +0200

    Add serverside Module related functions declarations

--- src/server.d.ts
+++ src/server.d.ts
@@ -3869,14 +3869,6 @@ declare function getDeadPlayers(): object;
 declare function getJetpackWeaponEnabled(weapon: string): boolean;
 
 /**
- * This function returns all the currently loaded modules of the server.
-
- * @returns Returns a table of all the currently loaded modules. If no modules are loaded, the table will be empty.
- * @see https://wiki.mtasa.com/wiki/GetLoadedModules
-**/
-declare function getLoadedModules(): object;
-
-/**
  * This function returns the maximum number of player slots on the server.
 
  * @returns Returns the maximum number of players allowed on the server.
@@ -3892,14 +3884,6 @@ declare function getMaxPlayers(): number;
  * @see https://wiki.mtasa.com/wiki/GetModelHandling
 **/
 declare function getModelHandling(modelId: number): object;
-
-/**
- * This function returns information about the specified module.
- * @param moduleName A string containing the module you wish to get information of e.g."hashing.dll"
- * @returns Returns a table containing information about module. These keys are present in the table: If invalid name for module is passed, it will return false.
- * @see https://wiki.mtasa.com/wiki/GetModuleInfo
-**/
-declare function getModuleInfo(moduleName: string): object;
 
 /**
  * This function returns the current gravity for the specified ped.
