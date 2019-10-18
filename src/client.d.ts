a8c3a930888847d402abf494fec25235d3074f4b
Evgeniy Shtro <augustalonso@gmail.com>
2019-10-18 03:37:25 +0200

    Add clientside Projectile related functions declarations

--- src/client.d.ts
+++ src/client.d.ts
@@ -459,30 +459,6 @@ declare function createPed(modelid: number, x: number, y: number, z: number, rot
 declare function createPickup(x: number, y: number, z: number, theType: number, amount/weapon: number, ,: unknown, respawnTime?: number, ammo?: number): Pickup;
 
 /**
- * This function creates a projectile of the specified type on the specified coordinates.
- * - Note: *Model argument is not synchronized between clients. Clients differs from local player see always standard projectile model.
- * *Target argument can only be defined as a player or another projectile.
- * @param creator The element representing creator of the projectile. In case you want the projectile to be synced for everybody creator must be the local player or his vehicle.
- * @param weaponType int representing the projectile weaponType (characteristics). Valid IDs are
- * @param posX unknown
- * @param posY unknown
- * @param posZ unknown
- * @param [force=1] float representing the starting force for throwable projectiles.
- * @param , unknown
- * @param [target=nil] element target used for heat seeking rockets.
- * @param rotX unknown
- * @param rotY unknown
- * @param rotZ unknown
- * @param velX unknown
- * @param velY unknown
- * @param velZ unknown
- * @param model Integer representing the projectile's model, uses default model for weaponType if not specified.
- * @returns Returns a projectile element if projectile creation was successful. Returns false if unable to create a projectile (wrong weapon ID or projectiles limit was reached).
- * @see https://wiki.mtasa.com/wiki/CreateProjectile
-**/
-declare function createProjectile(creator: Element, weaponType: number, posX: number, posY: number, posZ: number, force?: number, ,: unknown, target?: Element, rotX: number, rotY: number, rotZ: number, velX: number, velY: number, velZ: number, model: number): Projectile;
-
-/**
  * This function can be used to create custom radar areas on the radar.
  * @param startPosX A float representing the origin 'x' position of the radar area.
  * @param startPosY A float representing the origin 'y' position of the radar area.
@@ -652,13 +628,6 @@ declare function detachElements(theElement: Element, theAttachToElement: Element
  * @see https://wiki.mtasa.com/wiki/DetachTrailerFromVehicle
 **/
 declare function detachTrailerFromVehicle(theVehicle: Vehicle, theTrailer?: Vehicle): boolean;
-
-/**
- * This function can be used to detonate a players satchels.
- * @returns Returns true if successful, false otherwise.
- * @see https://wiki.mtasa.com/wiki/DetonateSatchels
-**/
-declare function detonateSatchels(): boolean;
 
 /**
  * New items: 3.0140,1.4,This function ensures the requested resource file is correct and then triggers onClientFileDownloadComplete. If the file has been previously downloaded and the CRC matches, the file will not be downloaded again but onClientFileDownloadComplete will still run. The file should also be included in the resource meta.xml with the download attribute set to "false", see meta.xml for more details.
@@ -1908,46 +1877,6 @@ declare function getPlayerWantedLevel(getPlayerWantedLevel: number): [];
  * @see https://wiki.mtasa.com/wiki/GetPlayersInTeam
 **/
 declare function getPlayersInTeam(theTeam: Team): object;
-
-/**
- * Get the time left before a projectile detonates.
- * @param projectile the projectile to get the timer of.
- * @returns Returns the the time in milliseconds to detonation which depending on the projectile type will do different things:
- * @see https://wiki.mtasa.com/wiki/GetProjectileCounter
-**/
-declare function getProjectileCounter(projectile: Projectile): number;
-
-/**
- * This function returns the creator of the specified projectile.
- * @param theProjectile The projectile element which creator you want to retrieve.
- * @returns Returns the element which created the projectile if successful, false otherwise.
- * @see https://wiki.mtasa.com/wiki/GetProjectileCreator
-**/
-declare function getProjectileCreator(theProjectile: Projectile): Element;
-
-/**
- * This function returns the force of the specified projectile.
- * @param theProjectile The projectile element which force you want to retrieve.
- * @returns Returns a float if successful, false otherwise.
- * @see https://wiki.mtasa.com/wiki/GetProjectileForce
-**/
-declare function getProjectileForce(theProjectile: Projectile): number;
-
-/**
- * This function returns the target of the specified projectile.
- * @param theProjectile The projectile element which target you want to retrieve.
- * @returns Returns the element which is the projectile's target if the projectile is valid and can have a target (like a heat-seeking rocket), false otherwise. If the projectile is a satchel charge, returns the element at which it is glued to (or nil if it isn't glued to any).
- * @see https://wiki.mtasa.com/wiki/GetProjectileTarget
-**/
-declare function getProjectileTarget(theProjectile: Projectile): Element;
-
-/**
- * This function returns the type of the specified projectile.
- * @param theProjectile The projectile element which type you want to retrieve.
- * @returns Returns an integer over the type of the projectile or false if invalid arguments were passed.
- * @see https://wiki.mtasa.com/wiki/GetProjectileType
-**/
-declare function getProjectileType(theProjectile: Projectile): number;
 
 /**
  * This function can be used to retrieve the current color of a radar area.
@@ -4447,15 +4376,6 @@ declare function setPlayerNametagShowing(thePlayer: Player, showing: boolean): b
  * @see https://wiki.mtasa.com/wiki/SetPlayerNametagText
 **/
 declare function setPlayerNametagText(thePlayer: Player, text: string): boolean;
-
-/**
- * Will change the projectile counter timer which depending on the projectile type will do different things:
- * @param projectile The projectile to edit the timer of.
- * @param timeToDetonate The time in milliseconds to detonation.
- * @returns Returns true on success, false otherwise.
- * @see https://wiki.mtasa.com/wiki/SetProjectileCounter
-**/
-declare function setProjectileCounter(projectile: Projectile, timeToDetonate: number): boolean;
 
 /**
  * Sets the color of an existing radar area.
